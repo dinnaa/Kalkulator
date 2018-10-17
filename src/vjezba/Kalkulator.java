0f6567ceb948d1aa61bf4a32a3b2fc730570a625
Edina Hodzic <edina_17_78@hotmail.com>
2018-10-17 16:30:29 +0200

    Implementiranje metoda

---
+++ src/vjezba/Kalkulator.java
@@ -0,0 +1,18 @@
+package vjezba;
+
+public class Kalkulator {
+
+		public double mnozenje(double a, double b) {
+			return 0;
+		}
+		public  double dijeljenje(double a, double b) {
+			return 0;
+		}
+		public  double korjenovanje(double broj) {
+			return 0;
+		}
+			public  double kvadriranje(double broj1) {
+				return 0;
+			}
+
+}
