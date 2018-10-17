14142671198f4e1a7027b5b533c7ee209e29939a
Azra Ademovic <azra,ademovic96master@outlook.com>
2018-10-17 17:22:45 +0200

    Implementirana metoda oduzimanje

--- src/vjezba/Kalkulator.java
+++ src/vjezba/Kalkulator.java
@@ -14,5 +14,12 @@ public class Kalkulator {
 			public  double kvadriranje(double broj1) {
 				return 0;
 			}
+			
+			public  double Oduzimanje(double broj1 , double broj2) {
+				double razlika = broj1 -  broj2;
+				System.out.println("Razlika dva broja je " + razlika);
+				return razlika;
+			}
+
 
 }
