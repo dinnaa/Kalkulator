cbaaa493ce0c1c2e46a47305c580cec4d29ec92f
Amra Mhemedovic <mehmedovic.amra@yahoo.com>
2018-10-18 15:33:47 +0200

    Implementirala korjenovanje i kvadridanje

--- src/vjezba/Kalkulator.java
+++ src/vjezba/Kalkulator.java
@@ -10,10 +10,10 @@ public class Kalkulator {
 			return a / b;
 		}
 		public  double korjenovanje(double broj) {
-			return 0;
+			return Math.sqrt(a)
 		}
 			public  double kvadriranje(double broj1) {
-				return 0;
+				return Math.pow(a, 2);
 			}
 			public  double Sabiranje (double broj1 , double broj2) {
 				double zbir = broj1 + broj2;
